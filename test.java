316944d497ab8df58b80d2c33acc89f3b50bd109
Ola Wethal Petersen <olawp@hiof.no>
2018-05-03 23:00:12 +0200

    Auto stash before merge of "master" and "origin/master"

--- test.java
+++ test.java
@@ -1,3 +1,16 @@
 public class SebErHomo {
-	
+	private String navn;
+	private int alder;
+	private boolean erHomo = true;
+
+	public SebErHomo(String navn, int alder, boolean erHomo){
+		this.navn = navn;
+		this.alder = alder
+		this.erHomo = erHomo;
+	}
+
+	public void lagHomo(String navn, int alder, boolean erHomo){
+		nyHomo = new SebErHomo("Sebastian",69,true);
+	}
+
 }
